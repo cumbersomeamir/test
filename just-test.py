0244be9420c5a242ff45ab4b6db3eb95ad56b96b
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-05-04 20:48:32 +0100

    Update just-test.py

--- just-test.py
+++ just-test.py
@@ -2,14 +2,7 @@
 #pip3 install pandas datasets transformers 
 import pandas as pd
 from datasets import Dataset
-from transformers import (
-    AutoTokenizer,
-    AutoModelForCausalLM,
-    DataCollatorForLanguageModeling,
-    DeepSpeedPlugin,
-    Trainer,
-    TrainingArguments,
-)
+from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForLanguageModeling, DeepSpeedPlugin, Trainer, TrainingArguments
 
 # Reading the file
 data = pd.read_excel("MedQuad dataset test.xlsx")
