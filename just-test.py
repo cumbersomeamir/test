b42d9ae25940071ce710a78fe4d203baf4417db4
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-05-04 20:46:25 +0100

    Update just-test.py

--- just-test.py
+++ just-test.py
@@ -1,9 +1,15 @@
 # Importing the relevant libraries
-#pip3 install transformers pandas openpyxl datasets sentencepiece
-import torch
+#pip3 install pandas datasets transformers 
 import pandas as pd
 from datasets import Dataset
-from transformers import TrainingArguments, Trainer, AutoTokenizer, AutoModelForCausalLM
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    DataCollatorForLanguageModeling,
+    DeepSpeedPlugin,
+    Trainer,
+    TrainingArguments,
+)
 
 # Reading the file
 data = pd.read_excel("MedQuad dataset test.xlsx")
@@ -17,9 +23,9 @@ tokenizer.pad_token = tokenizer.eos_token
 
 # Tokenization
 def tokenize_function(examples):
-    tokenized_prompt = tokenizer(examples['prompt'], truncation=True, padding='max_length', max_length=128, return_tensors='pt')
-    tokenized_completion = tokenizer(examples['completion'], truncation=True, padding='max_length', max_length=128, return_tensors='pt')
-    return {'input_ids': tokenized_prompt['input_ids'], 'attention_mask': tokenized_prompt['attention_mask'], 'labels': tokenized_completion['input_ids']}
+    tokenized_prompt = tokenizer(examples["prompt"], truncation=True, padding="max_length", max_length=128, return_tensors="pt")
+    tokenized_completion = tokenizer(examples["completion"], truncation=True, padding="max_length", max_length=128, return_tensors="pt")
+    return {"input_ids": tokenized_prompt["input_ids"], "attention_mask": tokenized_prompt["attention_mask"], "labels": tokenized_completion["input_ids"]}
 
 tokenized_dataset = hf_dataset.map(tokenize_function, batched=True)
 print("The tokenized dataset is ", tokenized_dataset)
@@ -27,35 +33,32 @@ print("The tokenized dataset is ", tokenized_dataset)
 # Load the pre-trained GPT-Neo 1.3B model
 model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6b")
 
-# Get the number of available GPUs
-num_gpus = torch.cuda.device_count()
-
 # Define the training arguments
 training_args = TrainingArguments(
-    output_dir='output',
+    output_dir="output",
     num_train_epochs=3,
     per_device_train_batch_size=2,
     save_total_limit=1,
     logging_steps=100,
     evaluation_strategy="no",
-    n_gpu=num_gpus,  # Set the number of GPUs
+    deepspeed="./ds_config.json",  # Use DeepSpeed configuration
 )
 
-# Create a Trainer instance
+    
+    # Create a Trainer instance
 trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset,
-)
-
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset,
+    )
 # Fine-tune the model
 trainer.train()
 
 # Save the fine-tuned model
-trainer.save_model('finetuned-gptj-6B')
+trainer.save_model("finetuned-gptj-6B")
 
-model = AutoModelForCausalLM.from_pretrained('finetuned-gptj-6B')
+model = AutoModelForCausalLM.from_pretrained("finetuned-gptj-6B")
 
-# Saving the Model on huggingface
+# Saving the Model on Hugging Face
 token = "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"
 model.push_to_hub("Amirkid/finetune-llama-test", use_auth_token=token)
